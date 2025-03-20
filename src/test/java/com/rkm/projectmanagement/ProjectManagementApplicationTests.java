ee18f0543253a9f3dd37f72426fcd608fc853029
Mas.gan <reikan.kresna@gmail.com>
2025-03-20 22:31:34 +0700

    This is the first commit of vendor-project-management

---
+++ src/test/java/com/rkm/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkm.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
